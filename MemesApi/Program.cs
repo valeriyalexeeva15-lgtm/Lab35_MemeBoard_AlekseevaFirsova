a1702c0b1cf47dcc7d6e62b09c39c9891e0dc269
karasidnez <karasdnez@gmail.com>
2026-04-29 14:01:23 +0300

    Step 2: MemesApi with CORS configured

--- MemesApi/Program.cs
+++ MemesApi/Program.cs
@@ -1,17 +1,31 @@
+using System.Text.Json;
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(FileOptions =>
+     {
+         FileOptions.JsonSerializerOptions.PropertyNamingPolicy =
+         JsonNamingPolicy.CamelCase;
+     });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => {
+    options.AddPolicy ("AllowAll", policy => {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 var app = builder.Build();
 
-if (app. Environment.IsDevelopment()) {
+if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app. UseAuthorization();
+app.UseCors("AllowAll");
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
